1fe52354680a83a945fe53182e30d9a44665db0f
Riley Ku <life10135658@gmail.com>
2024-06-22 16:51:40 +0800

    print debug message

--- app.py
+++ app.py
@@ -4,37 +4,22 @@ import pandas as pd
 from flask import Flask, request, abort
 from backend.build import *
 
-from linebot.v3 import (
-    WebhookHandler
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage,
-    ButtonsTemplate, DatetimePickerTemplateAction, PostbackEvent, PostbackTemplateAction,
-    MessageAction, QuickReply, QuickReplyButton
+    ButtonsTemplate, DatetimePickerTemplateAction, PostbackEvent,
+    PostbackTemplateAction, MessageAction, QuickReply, QuickReplyButton
 )
+
 
 app = Flask(__name__)
 
 channel_access_token = os.environ.get('channel_access_token')
 channel_secret = os.environ.get('channel_secret')
 
-configuration = Configuration(access_token=channel_access_token)
+line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
-
 
 create_table()
 
@@ -58,28 +43,18 @@ def callback():
     return 'OK'
 
 # handler在收到事件後，會根據定義的行為做相對應的處理
-@handler.add(MessageEvent, message=TextMessageContent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message_input = event.message.text
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        if message_input == 'test':
-            # line_bot_api.reply_message(event.reply_token,TextSendMessage(text='test'))
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text='test')],
-                ))
-        elif message_input == 'music':
-            jazz = get_data('爵士')
-            # line_bot_api.reply_message(event.reply_token,TextSendMessage(text=jazz))
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text=jazz)],
-                ))
+    print(event.reply_token)
+    if message_input == 'test':
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='test'))
+    elif message_input == 'music':
+        jazz = get_data('爵士')
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=jazz))
+
 
 # 當py檔案被直接執行時，__name__變數會是__main__，因此當此條件成立時，代表程式被當作主程式執行，而不是被當作模組引用。
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=5000)
